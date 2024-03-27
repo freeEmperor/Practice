cb3fd77d9d0f1701bb2747039422c910b60c9946
freeEmperor <164176555+freeEmperor@users.noreply.github.com>
2024-03-27 09:25:29 +0800

    Add files via upload

---
+++ front-end/js/register.js
@@ -0,0 +1,39 @@
+const but = document.querySelector('.keyin button')
+but.addEventListener('click',function(){
+    const hao = document.querySelector('.hao').value
+    const mi = document.querySelector('.mi').value
+    const qmi = document.querySelector('.qmi').value
+    let txt = document.querySelector('.clue')
+    if(hao == false || mi == false){
+        txt.innerHTML = '注册信息不能留空'
+        txt.style.color = 'darkred'
+        return
+    }
+    if(mi !== qmi){
+        txt.innerHTML = '两次输入的密码不一致'
+        txt.style.color = 'darkred'
+        return
+    }
+    let users = JSON.parse(localStorage.Users)
+    let i
+    for(i = 0 ; i< users.length ; i++ ){
+        if(users[i].user === hao){
+            txt.innerHTML = '用户名已被占用'
+            txt.style.color = 'darkred'
+            return
+        }
+    }
+    const ob = {
+        user: hao,
+        password: mi,
+        article: 0,
+        fans: 0,
+        subscribe: 0
+    }
+    users.push(ob)
+    const strob = JSON.stringify(ob);
+    localStorage.Users = strob
+
+    txt.innerHTML = '注册成功！快去登录吧~'
+    txt.style.color = 'black'
+})
